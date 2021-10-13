e68d5708d5519f53868ccfe20bbe383131d9a9e6
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-10-13 17:18:38 +0300

    feat: task1. Add 2 objects

---
+++ main.js
@@ -0,0 +1,19 @@
+const player1 = {
+  name: "Scorpion",
+  hp: 100,
+  img: "",
+  weapon: ["knife", "gun", "suriken"],
+  attack: function () {
+    console.log(`${this.name} Fight`);
+  },
+};
+
+const player2 = {
+  name: "Sonya",
+  hp: 100,
+  img: "",
+  weapon: ["knife", "gun", "suriken"],
+  attack: function () {
+    console.log(`${this.name} Fight`);
+  },
+};
