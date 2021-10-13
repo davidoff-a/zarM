c4955352447bce356f4582e0025390326e329fbb
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-10-13 18:54:38 +0300

    feat: task2. pass variables into function CreatePlayer

--- main.js
+++ main.js
@@ -18,22 +18,22 @@ const player2 = {
   },
 };
 
-function createPlayer() {
+function createPlayer(player, heroName, hitPoints) {
   const $arenas = document.querySelector(".arenas");
-  const $player = document.createElement("div.player1");
-  $player.classList.add("Player1");
+  const $player = document.createElement("div");
+  $player.classList.add(`${player}`);
   const $progressbar = document.createElement("div");
   $progressbar.classList.add("progressbar");
   const $life = document.createElement("div");
   $life.classList.add("life");
-  $life.style.width = "100%";
+  $life.style.width = `${hitPoints}%`;
   const $name = document.createElement("div");
   $name.classList.add("name");
-  $name.innerText = "Scorpion";
+  $name.innerText = `${heroName}`;
   const $character = document.createElement("div");
   $character.classList.add("character");
   const $charImg = document.createElement("img");
-  $charImg.src = "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif";
+  $charImg.src = `http://reactmarathon-api.herokuapp.com/assets/${heroName}.gif`;
   $character.appendChild($charImg);
   $progressbar.appendChild($life);
   $progressbar.appendChild($name);
@@ -42,4 +42,5 @@ function createPlayer() {
   $arenas.appendChild($player);
 }
 
-createPlayer();
+createPlayer("player1", "scorpion", 50);
+createPlayer("player2", "kitana", 75);
