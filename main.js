2d8b376360b4707c626568649deaa834073ef309
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-10-13 18:35:16 +0300

    feat: task1. add function Create Player

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ const player2 = {
     console.log(`${this.name} Fight`);
   },
 };
+
+function createPlayer() {
+  const $player = document.createElement("div");
+  $player.classList.add("Player1");
+  const $progressbar = document.createElement("div");
+  $progressbar.classList.add("progressbar");
+  const $life = document.createElement("div");
+  $life.classList.add("life");
+  $life.style.width = "100%";
+  const $name = document.createElement("div");
+  $name.classList.add("name");
+  $name.innerText = "Scorpion";
+  const $character = document.createElement("div");
+  $character.classList.add("character");
+  const $charImg = document.createElement("img");
+  $charImg.src = "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif";
+  $progressbar.appendChild($life, $name);
+  $player.appendChild($progressbar, $character);
+}
