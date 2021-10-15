cdc14854fd477ccb2ac3892344670792d10be478
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-10-15 13:36:07 +0300

    feat: add changes from video: add buttonadd status of loser, refactor function of html creation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const $arenas = document.querySelector(".arenas");
+const $randomButton = document.querySelector(".button");
 const player1 = {
   player: 1,
   name: "scorpion",
@@ -12,7 +13,7 @@ const player1 = {
 const player2 = {
   player: 2,
   name: "sonya",
-  hp: 100,
+  hp: 80,
   img: "",
   weapon: ["knife", "gun", "suriken"],
   attack: function () {
@@ -20,20 +21,15 @@ const player2 = {
   },
 };
 
-function createPlayer(classes, player) {
-  const $player = document.createElement("div");
-  $player.classList.add(`${classes}`);
-  const $progressbar = document.createElement("div");
-  $progressbar.classList.add("progressbar");
-  const $life = document.createElement("div");
-  $life.classList.add("life");
+function createPlayer(playerClass, player) {
+  const $player = createElement("div", playerClass);
+  const $progressbar = createElement("div", "progressbar");
+  const $life = createElement("div", "life");
   $life.style.width = `${player.hp}%`;
-  const $name = document.createElement("div");
-  $name.classList.add("name");
+  const $name = createElement("div", "name");
   $name.innerText = `${player.name}`;
-  const $character = document.createElement("div");
-  $character.classList.add("character");
-  const $charImg = document.createElement("img");
+  const $character = createElement("div", "character");
+  const $charImg = createElement("img");
   $charImg.src = `http://reactmarathon-api.herokuapp.com/assets/${player.name}.gif`;
   $character.appendChild($charImg);
   $progressbar.appendChild($life);
@@ -45,3 +41,31 @@ function createPlayer(classes, player) {
 
 createPlayer("player1", player1);
 createPlayer("player2", player2);
+
+function changeHP(player) {
+  const $playerLife = document.querySelector(`.player${player.player} .life`);
+  player.hp -= 20;
+  $playerLife.style.width = player.hp + "%";
+  if (player.hp < 0) {
+    $arenas.appendChild(playerLose(player.name));
+  }
+}
+
+function playerLose(name) {
+  const $loseTitle = createElement("div", "loseTitle");
+  $loseTitle.innerText = `${name} lose`;
+  return $loseTitle;
+}
+$randomButton.addEventListener("click", () => {
+  console.log("Hello world!");
+  changeHP(player1);
+  changeHP(player2);
+});
+
+function createElement(tag, className) {
+  const $element = document.createElement(tag);
+  if (className && className.length > 0) {
+    $element.classList.add(className);
+  }
+  return $element;
+}
