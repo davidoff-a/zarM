b6b67b0354963a75ec70d6c02ffe6fc2be691d6c
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-10-22 20:42:45 +0300

    feat: add function playerAttack

--- main.js
+++ main.js
@@ -32,7 +32,46 @@ const player2 = {
   elHP,
   renderHP,
 };
-
+const logs = {
+  start:
+    "Часы показывали [time], когда [player1] и [player2] бросили вызов друг другу.",
+  end: [
+    "Результат удара [playerWins]: [playerLose] - труп",
+    "[playerLose] погиб от удара бойца [playerWins]",
+    "Результат боя: [playerLose] - жертва, [playerWins] - убийца",
+  ],
+  hit: [
+    "[playerDefence] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.",
+    "[playerDefence] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.",
+    "[playerDefence] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.",
+    "[playerDefence] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.",
+    "[playerDefence] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.",
+    "[playerDefence] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.",
+    "[playerDefence] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.",
+    "[playerDefence] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.",
+    "[playerDefence] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.",
+    "[playerDefence] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.",
+    "[playerDefence] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.",
+    "[playerDefence] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.",
+    "[playerDefence] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.",
+    "[playerDefence] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.",
+    "[playerDefence] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.",
+    "[playerDefence] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.",
+    "[playerDefence] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.",
+    "[playerDefence] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.",
+  ],
+  defense: [
+    "[playerKick] потерял момент и храбрый [playerDefence] отпрыгнул от удара открытой ладонью в ключицу.",
+    "[playerKick] не контролировал ситуацию, и потому [playerDefence] поставил блок на удар пяткой в правую грудь.",
+    "[playerKick] потерял момент и [playerDefence] поставил блок на удар коленом по селезенке.",
+    "[playerKick] поскользнулся и задумчивый [playerDefence] поставил блок на тычок головой в бровь.",
+    "[playerKick] старался провести удар, но непобедимый [playerDefence] ушел в сторону от удара копчиком прямо в пятку.",
+    "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+    "[playerKick] не думал о бое, потому расстроенный [playerDefence] отпрыгнул от удара кулаком куда обычно не бьют.",
+    "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+  ],
+  draw: "Ничья - это тоже победа!",
+};
 function createPlayer(player) {
   const $player = createElement("div", `player${player.player}`);
   const $progressbar = createElement("div", "progressbar");
@@ -87,20 +126,9 @@ function playerWins(name) {
 $frmControl.addEventListener("submit", (event) => {
   event.preventDefault();
   const ENEMY = enemyAttack();
-  const MY_ATTACK = {};
-  for (let item of $frmControl) {
-    if (item.checked && item.name === "hit") {
-      MY_ATTACK.hitPoints = getRandomNumber(RANDOMIZE_MIN, HIT[item.value]);
-      MY_ATTACK.hit = item.value;
-    }
-
-    if (item.checked && item.name === "defense") {
-      MY_ATTACK.defense = item.value;
-    }
-    item.checked = false;
-  }
-  renderFight.apply(player1, [MY_ATTACK, ENEMY]);
-  renderFight.apply(player2, [ENEMY, MY_ATTACK]);
+  const PLAYER = playerAttack();
+  renderFight.apply(player1, [PLAYER, ENEMY]);
+  renderFight.apply(player2, [ENEMY, PLAYER]);
   let winner = determineWinner();
   if (winner) {
     declareWinner(winner);
@@ -190,6 +218,21 @@ function renderFight(attackerParams, defenderParams) {
   }
 }
 
+function playerAttack() {
+  const MY_ATTACK = {};
+  for (let item of $frmControl) {
+    if (item.checked && item.name === "hit") {
+      MY_ATTACK.hitPoints = getRandomNumber(RANDOMIZE_MIN, HIT[item.value]);
+      MY_ATTACK.hit = item.value;
+    }
+
+    if (item.checked && item.name === "defense") {
+      MY_ATTACK.defense = item.value;
+    }
+    item.checked = false;
+  }
+  return MY_ATTACK;
+}
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
 $arenas.appendChild(createReloadButton());
