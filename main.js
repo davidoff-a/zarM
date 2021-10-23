98f58f63ceac75b6872b188aa26b362abf33ecbe
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-10-23 14:33:44 +0300

    feat: add logging action to the app with switch case operator

--- main.js
+++ main.js
@@ -41,38 +41,39 @@ const logs = {
     "Результат боя: [playerLose] - жертва, [playerWins] - убийца",
   ],
   hit: [
-    "[playerDefence] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.",
-    "[playerDefence] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.",
-    "[playerDefence] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.",
-    "[playerDefence] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.",
-    "[playerDefence] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.",
-    "[playerDefence] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.",
-    "[playerDefence] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.",
-    "[playerDefence] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.",
-    "[playerDefence] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.",
-    "[playerDefence] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.",
-    "[playerDefence] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.",
-    "[playerDefence] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.",
-    "[playerDefence] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.",
-    "[playerDefence] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.",
-    "[playerDefence] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.",
-    "[playerDefence] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.",
-    "[playerDefence] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.",
-    "[playerDefence] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.",
+    "[playerDefense] пытался сконцентрироваться, но [playerKick] разбежавшись раздробил копчиком левое ухо врага.",
+    "[playerDefense] расстроился, как вдруг, неожиданно [playerKick] случайно раздробил грудью грудину противника.",
+    "[playerDefense] зажмурился, а в это время [playerKick], прослезившись, раздробил кулаком пах оппонента.",
+    "[playerDefense] чесал <вырезано цензурой>, и внезапно неустрашимый [playerKick] отчаянно размозжил грудью левый бицепс оппонента.",
+    "[playerDefense] задумался, но внезапно [playerKick] случайно влепил грубый удар копчиком в пояс оппонента.",
+    "[playerDefense] ковырялся в зубах, но [playerKick] проснувшись влепил тяжелый удар пальцем в кадык врага.",
+    "[playerDefense] вспомнил что-то важное, но внезапно [playerKick] зевнув, размозжил открытой ладонью челюсть противника.",
+    "[playerDefense] осмотрелся, и в это время [playerKick] мимоходом раздробил стопой аппендикс соперника.",
+    "[playerDefense] кашлянул, но внезапно [playerKick] показав палец, размозжил пальцем грудь соперника.",
+    "[playerDefense] пытался что-то сказать, а жестокий [playerKick] проснувшись размозжил копчиком левую ногу противника.",
+    "[playerDefense] забылся, как внезапно безумный [playerKick] со скуки, влепил удар коленом в левый бок соперника.",
+    "[playerDefense] поперхнулся, а за это [playerKick] мимоходом раздробил коленом висок врага.",
+    "[playerDefense] расстроился, а в это время наглый [playerKick] пошатнувшись размозжил копчиком губы оппонента.",
+    "[playerDefense] осмотрелся, но внезапно [playerKick] робко размозжил коленом левый глаз противника.",
+    "[playerDefense] осмотрелся, а [playerKick] вломил дробящий удар плечом, пробив блок, куда обычно не бьют оппонента.",
+    "[playerDefense] ковырялся в зубах, как вдруг, неожиданно [playerKick] отчаянно размозжил плечом мышцы пресса оппонента.",
+    "[playerDefense] пришел в себя, и в это время [playerKick] провел разбивающий удар кистью руки, пробив блок, в голень противника.",
+    "[playerDefense] пошатнулся, а в это время [playerKick] хихикая влепил грубый удар открытой ладонью по бедрам врага.",
   ],
   defense: [
-    "[playerKick] потерял момент и храбрый [playerDefence] отпрыгнул от удара открытой ладонью в ключицу.",
-    "[playerKick] не контролировал ситуацию, и потому [playerDefence] поставил блок на удар пяткой в правую грудь.",
-    "[playerKick] потерял момент и [playerDefence] поставил блок на удар коленом по селезенке.",
-    "[playerKick] поскользнулся и задумчивый [playerDefence] поставил блок на тычок головой в бровь.",
-    "[playerKick] старался провести удар, но непобедимый [playerDefence] ушел в сторону от удара копчиком прямо в пятку.",
-    "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
-    "[playerKick] не думал о бое, потому расстроенный [playerDefence] отпрыгнул от удара кулаком куда обычно не бьют.",
-    "[playerKick] обманулся и жестокий [playerDefence] блокировал удар стопой в солнечное сплетение.",
+    "[playerKick] потерял момент и храбрый [playerDefense] отпрыгнул от удара открытой ладонью в ключицу.",
+    "[playerKick] не контролировал ситуацию, и потому [playerDefense] поставил блок на удар пяткой в правую грудь.",
+    "[playerKick] потерял момент и [playerDefense] поставил блок на удар коленом по селезенке.",
+    "[playerKick] поскользнулся и задумчивый [playerDefense] поставил блок на тычок головой в бровь.",
+    "[playerKick] старался провести удар, но непобедимый [playerDefense] ушел в сторону от удара копчиком прямо в пятку.",
+    "[playerKick] обманулся и жестокий [playerDefense] блокировал удар стопой в солнечное сплетение.",
+    "[playerKick] не думал о бое, потому расстроенный [playerDefense] отпрыгнул от удара кулаком куда обычно не бьют.",
+    "[playerKick] обманулся и жестокий [playerDefense] блокировал удар стопой в солнечное сплетение.",
   ],
   draw: "Ничья - это тоже победа!",
 };
 const $CHAT = document.querySelector(".chat");
+
 function createPlayer(player) {
   const $player = createElement("div", `player${player.player}`);
   const $progressbar = createElement("div", "progressbar");
@@ -124,18 +125,6 @@ function playerWins(name) {
   return $winsTitle;
 }
 
-$frmControl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const ENEMY = enemyAttack();
-  const PLAYER = playerAttack();
-  renderFight.apply(player1, [PLAYER, ENEMY]);
-  renderFight.apply(player2, [ENEMY, PLAYER]);
-  let winner = determineWinner();
-  if (winner) {
-    declareWinner(winner);
-  }
-});
-
 function createElement(tag, className) {
   const $element = document.createElement(tag);
   if (className && className.length > 0) {
@@ -146,10 +135,10 @@ function createElement(tag, className) {
 
 function determineWinner() {
   if (player1.hp && !player2.hp) {
-    return player1.name;
+    return player1;
   }
   if (!player1.hp && player2.hp) {
-    return player2.name;
+    return player2;
   }
   if (!player1.hp && !player2.hp) {
     return "Double Kill!";
@@ -160,11 +149,12 @@ function getRandomNumber(min, max) {
   return Math.ceil(Math.random() * (max - min) + min);
 }
 
-function declareWinner(winnerName) {
-  $arenas.appendChild(playerWins(winnerName));
+function declareWinner(winner) {
+  $arenas.appendChild(playerWins(winner.name));
   const $restartBtn = document.querySelector(".reloadWrap .button");
   $restartBtn.style.display = "block";
   $btnFight.disabled = true;
+  generateLogs("end", winner);
 }
 
 function createReloadButton() {
@@ -211,15 +201,6 @@ function getBangImg(numPlayer) {
   }, 1500);
 }
 
-function renderFight(attackerParams, defenderParams) {
-  if (checkBlocked(attackerParams, defenderParams)) {
-    this.changeHP(attackerParams.hitPoints);
-    getBangImg(this.player);
-    this.renderHP(this.elHP());
-    generateLog("hit", player2, player1);
-  }
-}
-
 function playerAttack() {
   const MY_ATTACK = {};
   for (let item of $frmControl) {
@@ -236,15 +217,76 @@ function playerAttack() {
   return MY_ATTACK;
 }
 
-function generateLog(type, attackPlayer, defensePlayer) {
+function generateLogs(type, attackPlayer, hitPoints = 0) {
   const LOG_RECORD_TIME = new Date().toLocaleTimeString();
-  const LOG_RECORD = logs[type][0]
-    .replace("[playerKick]", player1.name)
-    .replace("[playerDefence]", player2.name);
-  $CHAT.insertAdjacentHTML(
-    "afterbegin",
-    `<p>[${LOG_RECORD_TIME}] - ${LOG_RECORD}</p>`
-  );
+  const DEFENDER = attackPlayer.player === 1 ? player2 : player1;
+  let logRecord = "";
+  switch (type) {
+    case "start":
+      logRecord = logs[type]
+        .replace("[player1]", attackPlayer.name.toUpperCase())
+        .replace("[player2]", DEFENDER.name.toUpperCase())
+        .replace("[time]", LOG_RECORD_TIME);
+    case "hit":
+      logRecord = `${LOG_RECORD_TIME} - ${type} - ${logs[type][
+        getRandomNumber(RANDOMIZE_MIN, logs[type].length - 1)
+      ]
+        .replace("[playerKick]", attackPlayer.name.toUpperCase())
+        .replace(
+          "[playerDefense]",
+          DEFENDER.name.toUpperCase()
+        )} - ${hitPoints} - [${DEFENDER.hp}/100]`;
+      break;
+    case "defense":
+      logRecord = `${LOG_RECORD_TIME} - ${type} - ${logs[type][
+        getRandomNumber(RANDOMIZE_MIN, logs[type].length - 1)
+      ]
+        .replace("[playerKick]", attackPlayer.name.toUpperCase())
+        .replace("[playerDefense]", DEFENDER.name.toUpperCase())}`;
+      break;
+    case "end":
+      logRecord = logs[type][
+        getRandomNumber(RANDOMIZE_MIN, logs[type].length - 1)
+      ]
+        .replace("[playerWins]", attackPlayer.name.toUpperCase())
+        .replace("[playerLose]", DEFENDER.name.toLocaleUpperCase());
+      break;
+    case "draw":
+      logRecord =
+        logs[type][getRandomNumber(RANDOMIZE_MIN, logs[type].length - 1)];
+      break;
+  }
+  $CHAT.insertAdjacentHTML("afterbegin", `<p>${logRecord}</p>`);
+}
+
+$frmControl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const ENEMY = enemyAttack();
+  const PLAYER = playerAttack();
+  if ($CHAT.children.length === 0) {
+    generateLogs("start", player1);
+  }
+  renderFight.apply(player1, [PLAYER, ENEMY]);
+  renderFight.apply(player2, [ENEMY, PLAYER]);
+  let winner = determineWinner();
+  if (winner) {
+    declareWinner(winner);
+  }
+});
+
+function renderFight(attackerParams, defenderParams) {
+  if (checkBlocked(attackerParams, defenderParams)) {
+    this.changeHP(attackerParams.hitPoints);
+    getBangImg(this.player);
+    this.renderHP(this.elHP());
+    generateLogs(
+      "hit",
+      this.player === 1 ? player2 : player1,
+      attackerParams.hitPoints
+    );
+  } else {
+    generateLogs("defense", this.player === 1 ? player2 : player1);
+  }
 }
 
 $arenas.appendChild(createPlayer(player1));
