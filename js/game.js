2af79ea6a5fd2d2d41d2d5ef80ecd0ad74bb6e75
Алексей Давыдов <62559479+davidoff-a@users.noreply.github.com>
2021-10-31 13:50:48 +0300

    fix: fix some bugs

--- js/game.js
+++ js/game.js
@@ -10,10 +10,27 @@ let player2;
 class Game {
   constructor() {
     this.$ARENA = document.querySelector(".arenas");
+    this.$FORM = document.querySelector(".control");
+    this.$LOGO = document.querySelector(".logo");
   }
   init() {
     window.addEventListener("DOMContentLoaded", () => {
       this.$ARENA.classList.add(`arena${getRandomNumber(5, 1)}`);
+      const $SLIDE_DOOR_LEFT = document.querySelector(".wall-left");
+      const $SLIDE_DOOR_RIGHT = document.querySelector(".wall-right");
+      setTimeout(() => {
+        $SLIDE_DOOR_LEFT.classList.add("open");
+        $SLIDE_DOOR_RIGHT.classList.add("open");
+        setTimeout(() => {
+          this.$FORM.style.display = "flex";
+          this.$LOGO.display = "block";
+          const FIGHT_SOUND = new Audio("./assets/sound/fight/mk3-09020.mp3");
+          FIGHT_SOUND.play();
+        }, 5000);
+      }, 1500);
+      setTimeout(() => {
+        this.$LOGO.classList.add("off");
+      }, 3000);
     });
     this.start();
   }
