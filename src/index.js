bb997ab064ced7aa159181f4ec5154bc28be8a18
Davydau Aliaksei <davidoff.aleksei@gmail.com>
2022-06-03 01:10:50 +0300

    refactor: reorganize functions invoking in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { GAME } from "./js/game";
-import { buildWelcomePage, createEl } from "./js/buildHTML";
+import { buildWelcomePage } from "./js/buildHTML";
 
 import "./css/style.css";
 
 const leftWall = require("./assets/wall-left.png");
 const rightWall = require("./assets/wall-right.png");
 const logoImg = require("./assets/logo.png");
-const appInit = () => {
-  buildWelcomePage();
-};
-appInit();
+buildWelcomePage();
+
 GAME.init();
